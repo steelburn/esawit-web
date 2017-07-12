d3057fbb3fcc79334c125f213f7be14e6c4e0f28
Unknown <sampath415@gmail.com>
2017-07-12 14:08:39 +0800

    Reconsiliationfile

---
+++ src/pages/reconciliation/reconciliation.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ReconciliationPage } from './reconciliation';
+
+@NgModule({
+  declarations: [
+    ReconciliationPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ReconciliationPage),
+  ],
+  exports: [
+    ReconciliationPage
+  ]
+})
+export class ReconciliationPageModule {}
